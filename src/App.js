74fa75db453af060c8cd1f30bdbdbd1f3fe5b2ef
Habidhossen <habidhossen2@gmail.com>
2022-06-17 17:32:38 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-   
+      <Routes>{/* <Router path="/" element={<Home />} /> */}</Routes>
     </div>
   );
 }
