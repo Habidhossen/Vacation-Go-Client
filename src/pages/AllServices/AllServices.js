a33cba737f7f10836cca5f4f37f01e35ccc9e772
Habidhossen <habidhossen2@gmail.com>
2023-02-12 01:11:32 +0600

    services ui updated

--- src/pages/AllServices/AllServices.js
+++ src/pages/AllServices/AllServices.js
@@ -1,3 +1,4 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import Loader from "../Shared/Loader/Loader";
@@ -18,16 +19,21 @@ const AllServices = () => {
       <div className="grid grid-cols-3 gap-6">
         {services
           .map((service) => (
-            <div>
-              <div className="service-card">
-                <img className="service-card-img" src={service.imgURL} alt="" />
-                <div>
-                  <p className="service-card-price">${service.price}</p>
-                  <h1 className="service-card-title">{service.name}</h1>
-                  <p className="service-card-text">{service.desc}</p>
-                </div>
-                <Link to={`../service/${service._id}`} className="checkout-btn">
-                  Checkout
+            <div className="bg-white">
+              <img className="" src={service.imgURL} alt="" />
+              <div className="flex flex-col items-center p-4">
+                <h1 className="text-lg font-semibold">{service.name}</h1>
+                <p className="font-fredoka text-2xl text-primary mt-1">
+                  ${service.price}{" "}
+                  <span className="text-base text-gray-600">
+                    <del>${Number(service.price) + 100}</del>
+                  </span>
+                </p>
+                <Link
+                  to={`service/${service._id}`}
+                  className="flex items-center gap-2 mt-14 uppercase -tracking-tight text-sm font-bold text-secondary hover:text-primary duration-300"
+                >
+                  Checkout <FaLongArrowAltRight />
                 </Link>
               </div>
             </div>
--- src/pages/Home/Service.js
+++ src/pages/Home/Service.js
@@ -1,3 +1,4 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "../../assets/styles/style.css";
 
@@ -5,16 +6,23 @@ const Service = ({ service }) => {
   const { _id, name, price, desc, imgURL } = service;
 
   return (
-    <div className="service-card">
-      <img className="service-card-img" src={imgURL} alt="" />
-      <div>
-        <p className="service-card-price">${price}</p>
-        <h1 className="service-card-title">{name}</h1>
-        <p className="service-card-text">{desc}</p>
+    <div className="bg-white">
+      <img className="" src={imgURL} alt="" />
+      <div className="flex flex-col items-center p-4">
+        <h1 className="text-lg font-semibold">{name}</h1>
+        <p className="font-fredoka text-2xl text-primary mt-1">
+          ${price}{" "}
+          <span className="text-base text-gray-600">
+            <del>${Number(price) + 100}</del>
+          </span>
+        </p>
+        <Link
+          to={`service/${_id}`}
+          className="flex items-center gap-2 mt-14 uppercase -tracking-tight text-sm font-bold text-secondary hover:text-primary duration-300"
+        >
+          Checkout <FaLongArrowAltRight />
+        </Link>
       </div>
-      <Link to={`service/${_id}`} className="checkout-btn">
-        Checkout
-      </Link>
     </div>
   );
 };
