16383a0b1f3bb16ffef5ad5a652b67f68a079325
Habidhossen <habidhossen2@gmail.com>
2023-02-16 00:03:20 +0600

    theme updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,43 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      fredoka: ["Fredoka One", "cursive"],
-      mulish: ["Mulish", "sans-serif"],
+    extend: {
+      colors: {
+        primary: "#41c3e5",
+        secondary: "#ff7e4c",
+        heading: "#2a445f",
+        text: "#4e4c55",
+        blue: "#4e3bf0",
+        purple: "#803bf0",
+        yellow: "#fcc845",
+        lightGrey: "#fafafa",
+        lightBlue: "#eaf6f6",
+        lightOrange: "#feead1",
+      },
+      fontFamily: {
+        fredoka: ["Fredoka One", "cursive"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+      screens: {
+        /* sm: "576px",
+        // => @media (min-width: 576px) { ... }
+
+        md: "960px",
+        // => @media (min-width: 960px) { ... }
+
+        lg: "1440px",
+        // => @media (min-width: 1440px) { ... }
+        */
+
+        sm: "576px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+      },
     },
   },
   daisyui: {
