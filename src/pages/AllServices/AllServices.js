35ff9d9921bba355435c3b69942f80341b8628b4
Habidhossen <habidhossen2@gmail.com>
2023-03-13 22:09:25 +0600

    code added

--- src/pages/AllServices/AllServices.js
+++ src/pages/AllServices/AllServices.js
@@ -1,6 +1,5 @@
-import { FaLongArrowAltRight } from "react-icons/fa";
 import { useQuery } from "react-query";
-import { Link } from "react-router-dom";
+import Service from "../Home/Service";
 import Loader from "../Shared/Loader/Loader";
 
 const AllServices = () => {
@@ -18,26 +17,7 @@ const AllServices = () => {
     <div className="px-20 py-8">
       <div className="grid grid-cols-3 gap-6">
         {services.data
-          .map((service) => (
-            <div className="bg-white">
-              <img className="" src={service.imgURL} alt="" />
-              <div className="flex flex-col items-center p-4">
-                <h1 className="text-lg font-semibold">{service.name}</h1>
-                <p className="font-fredoka text-2xl text-primary mt-1">
-                  ${service.price}{" "}
-                  <span className="text-base text-gray-600">
-                    <del>${Number(service.price) + 100}</del>
-                  </span>
-                </p>
-                <Link
-                  to={`${service._id}`}
-                  className="flex items-center gap-2 mt-14 uppercase -tracking-tight text-sm font-bold text-secondary hover:text-primary duration-300"
-                >
-                  Checkout <FaLongArrowAltRight />
-                </Link>
-              </div>
-            </div>
-          ))
+          .map((service) => <Service key={service._id} service={service} />)
           .reverse()}
       </div>
     </div>
